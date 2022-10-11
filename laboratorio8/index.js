6715f52fad7279f6d4236c77559bceb7ad5e762a
OscarV002 <Oavs02gmail.com>
2022-10-11 21:38:06 +0200

    updated

---
+++ laboratorio8/index.js
@@ -0,0 +1,30 @@
+var cadena,acumulado;
+document.getElementById("output").addEventListener("click", clean);
+const btn=document.getElementById("Limpiar");
+
+function clean(){
+    document.getElementById("text").value = " "
+    document.getElementById("output").innerHTML = "";
+    
+    btn.hidden=true;
+}
+
+function send(){
+    cadena = document.getElementById("text").value;
+    acumulado=(contarCaracteres(cadena));
+    function contarCaracteres() {
+        const texto = document.getElementById('text').
+        value.trim().toLowerCase().split('');
+        const repeticiones = {};
+        texto.forEach( ( letra ) => {
+          repeticiones[ letra ] = ( repeticiones[ letra ] || 0 ) + 1;
+        });
+        for( let letra in repeticiones ) {
+          const text = `${ letra } = ${ repeticiones[ letra ] }<br>`;
+          document.getElementById('output').innerHTML += text;
+        }
+        
+    }
+    btn.hidden=false;
+  
+  }
